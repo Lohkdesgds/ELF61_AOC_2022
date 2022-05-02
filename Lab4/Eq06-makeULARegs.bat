cba6662e9072cb7bdf8dc0bbf4977e824b510bd3
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2022-05-02 00:41:37 -0300

    Renomeado -tb para _tb no E106-ULARegs_tb.vhd (Lab4)

--- Lab4/Eq06-makeULARegs.bat
+++ Lab4/Eq06-makeULARegs.bat
@@ -2,9 +2,9 @@ ghdl -a Eq06-ULA.vhd
 ghdl -a Eq06-reg16bits.vhd
 ghdl -a Eq06-RegFile.vhd
 ghdl -a Eq06-ULARegs.vhd
-ghdl -a Eq06-ULARegs-tb.vhd
+ghdl -a Eq06-ULARegs_tb.vhd
 ghdl -r ULARegs_TB --ieee-asserts=disable-at-0 --stop-time=1000ns --wave=Eq06-ULARegs.ghw
 :: gtkw Eq06-ULARegs.ghw --save Eq06-ULARegs.gtkw
 
 :: usando o outro app
-:: helper.exe "ghdl -a Eq06-ULA.vhd" "ghdl -a Eq06-reg16bits.vhd" "ghdl -a Eq06-RegFile.vhd" "ghdl -a Eq06-ULARegs.vhd" "ghdl -a Eq06-ULARegs-tb.vhd" "ghdl -r ULARegs_TB --ieee-asserts=disable-at-0 --stop-time=1000ns --wave=Eq06-ULARegs.ghw" "gtkw Eq06-ULARegs.ghw --save Eq06-ULARegs.gtkw"
+:: helper.exe "ghdl -a Eq06-ULA.vhd" "ghdl -a Eq06-reg16bits.vhd" "ghdl -a Eq06-RegFile.vhd" "ghdl -a Eq06-ULARegs.vhd" "ghdl -a Eq06-ULARegs_tb.vhd" "ghdl -r ULARegs_TB --ieee-asserts=disable-at-0 --stop-time=1000ns --wave=Eq06-ULARegs.ghw" "gtkw Eq06-ULARegs.ghw --save Eq06-ULARegs.gtkw"
